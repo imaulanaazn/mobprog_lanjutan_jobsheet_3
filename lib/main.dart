053e19918b516e87bef38863c37f79cf24a6049d
irham maulana <i.maulana.azn@gmail.com>
2024-04-07 23:07:42 +0700

    fixing the code to follow the jobsheet 4 rule

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Aplikasi Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      title: 'First App',
       home: Scaffold(
-        backgroundColor: Colors.pink[400],
+        backgroundColor: Colors.pinkAccent,
         appBar: AppBar(
+          backgroundColor: Colors.orangeAccent,
           title: const Text('Aplikasi Flutter'),
-          backgroundColor: Colors.red,
-        
         ),
         body: const Center(
-          child: Text(
-            'Selamat datang di Flutter',
-            style: TextStyle(
-              fontSize: 20,
-            ),
-            textAlign: TextAlign.center,
-          ),
+          child: Text('Hello Irham Maulana'),
         ),
       ),
     );
